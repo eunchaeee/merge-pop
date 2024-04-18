0b31e6254b95ea2575c54ac3fd79fd39b6036e29
Ellie <ecjeong@maxst.com>
2024-04-18 20:48:25 +0900

    move spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,38 +5,50 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private FruitData fruitData;
+    [SerializeField] private Transform leftWall;
+    [SerializeField] private Transform rightWall;
+    [SerializeField] private Camera cam;
 
     private GameObject currentFruit;
     private SpawnerState currentState;
 
+    private float xMin;
+    private float xMax;
+
     public enum SpawnerState
     {
-        HoldFruit,
+        Holding,
         Dropping
     }
 
     private void Start()
     {
+        xMin = leftWall.position.x;
+        xMax = rightWall.position.x;
         Spawn();
-        currentState = SpawnerState.HoldFruit;
+        currentState = SpawnerState.Holding;
     }
 
     private void Update()
     {
         switch (currentState)
         {
-            case SpawnerState.HoldFruit:
+            case SpawnerState.Holding:
+                float x = Mathf.Clamp(cam.ScreenToWorldPoint(Input.mousePosition).x, xMin, xMax);
+                transform.position = new Vector2(x, transform.position.y);
+                //Debug.Log(cam.ScreenToWorldPoint(Input.mousePosition));
                 currentFruit.transform.position = transform.position;
                 if (Input.GetMouseButtonDown(0))
                 {
                     currentFruit.GetComponent<Rigidbody2D>().gravityScale = 1f;
+                    currentFruit = null;
                     currentState = SpawnerState.Dropping;
                 }
-
+                
                 break;
             case SpawnerState.Dropping:
                 Spawn();
-                currentState = SpawnerState.HoldFruit;
+                currentState = SpawnerState.Holding;
                 break;
         }
 
